59070a76514ee506b3e01fc19c8b53979be47ba2
nonk123 <Sergeyii321@gmail.com>
2020-10-11 13:05:45 +0300

    Merge package-init.el into init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,25 @@
 
 ;;; Code:
 
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; Workaround for Emacs 26; GNU package archive won't work otherwise.
+(when (= emacs-major-version 26)
+  (defvar gnutls-algorithm-priority) ; avoid a warning in later versions
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+
+(package-initialize)
+
+;; Make sure `use-package' is always installed.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
 (setq custom-file "~/.emacs.d/custom.el")
 
 ;; Create the custom file unless it's already there. Vital for a new install.
