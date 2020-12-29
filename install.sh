d9c5dc49eff1182e7b6bce259cf583959939e041
nonk123 <Sergeyii321@gmail.com>
2020-12-29 14:14:50 +0300

    Finishing touches
    The last commit before switching to a new PC.

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# A dotfiles install script.
+#
+# It simply stows all the directories in the repository root.
+
+# Args:
+# $@ - extra args to pass to stow.
 
 for dir in `ls -d */`; do
     stow "$@" "$dir"
