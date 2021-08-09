41770557820aa4e430c32877792c4832456e6e9b
nonk123 <Sergeyii321@gmail.com>
2021-08-09 21:16:06 +0300

    Make EXWM more bearable

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -292,6 +292,10 @@
   "Return a lambda calling (execute-command COMMAND)."
   (lambda () (interactive) (execute-command command)))
 
+(use-package vterm
+  :init (defun vterm-new-session ()
+          (interactive) (vterm t)))
+
 (use-package exwm
   :init
   (add-hook 'exwm-update-class-hook
@@ -301,7 +305,9 @@
             (lambda ()
               (unless exwm-instance-name
                 (exwm-workspace-rename-buffer exwm-title))))
+
   (setq exwm-manage-configurations '((t char-mode t)))
+
   (setq
    exwm-input-global-keys
    (mapcar
@@ -315,39 +321,35 @@
       ("s-m" . split-window-right)
       ("s-x" . delete-window)
       ("s-q" . kill-current-buffer)
-      ("s-z" . bury-buffer)
+      ("s-z" . previous-buffer)
+      ("s-b" . switch-to-buffer)
       ("s-d" . execute-command)
       ("s-w" . exwm-floating-toggle-floating)
       ("s-f" . exwm-layout-toggle-fullscreen)
       ("s-i" . load-init)
+      ("s-<return>" . vterm-new-session)
+      ("s-p" . ,(lambda-run "mpc toggle"))
       ,@(mapcar (lambda (i)
                   `(,(format "s-%d" i) .
                     (lambda ()
                       (interactive)
                       (exwm-workspace-switch-create ,i))))
                 (number-sequence 0 9))
-      ,@(mapcar (lambda (i)
-                  `(,(format "C-s-%d" i) .
-                    (lambda ()
-                      (interactive)
-                      (exwm-workspace-move-window ,i))))
-                (number-sequence 0 9))
       ("<print>" . ,(lambda-run "screenshot region"))
-      ("M-<print>" . ,(lambda-run "screenshot window"))
       ("S-<print>" . ,(lambda-run "screenshot display")))))
-   (exwm-enable))
 
-(defun configure-font (&optional frame)
-  (set-frame-font "Hack 9" nil t))
+  (require 'exwm-systemtray)
+  (exwm-systemtray-enable)
+  (exwm-enable)
 
-(add-hook 'after-make-frame-functions #'configure-font)
-(add-hook 'server-after-make-frame-functions #'configure-font)
+  ;; Update EXWM keys if they are changed.
+  (pcase-dolist (`(,key . ,command) exwm-input-global-keys)
+    (exwm-input--set-key key command))
+  (exwm-input--update-global-prefix-keys))
 
+(set-frame-font "Hack 9" nil t)
+(fringe-mode (cons nil 1))
 (scroll-bar-mode -1)
-
-(use-package vterm
-  :init (defun vterm-new-session () (interactive) (vterm t))
-  :bind ("C-c RET" . vterm-new-session))
 
 (use-package color-theme-sanityinc-tomorrow
   :init (when first-load
