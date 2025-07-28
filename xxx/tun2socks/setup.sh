b85792b4f147c7a21d23f61698b1cd47474d1023
nonk123 <me@nonk.dev>
2025-07-28 20:03:55 +0300

    Update tun2socks scribbles to match reality

--- xxx/tun2socks/setup.sh
+++ xxx/tun2socks/setup.sh
@@ -7,7 +7,8 @@ ip addr add 198.18.0.1/15 dev tun0
 ip link set dev tun0 up
 
 ip route del default
-ip route add default via 198.18.0.1 dev tun0 metric 1
+ip route add 192.248.182.40 via 192.168.1.1 dev enp2s0 metric 1
+ip route add default via 198.18.0.1 dev tun0 metric 2
 ip route add default via 192.168.1.1 dev enp2s0 metric 10
 
 # start the tun2socks system service here...
